7fafa654b9022f68982072cac381285fa93485c4
Jon Anders Amundsen <jon.a.amundsen@gmail.com>
2015-11-27 14:38:12 +0100

    Some work on selecting an area of the image by mouse. Cant be used for anything yet

--- index.js
+++ index.js
@@ -4,15 +4,91 @@ var dialog = remote.require('dialog');
 var fs = require('fs');
 
 var canvas = document.getElementById('image-canvas');
-canvas.width = 0;
-canvas.height = 0;
+var selectionCanvas = document.getElementById('selection-canvas');
+canvas.width = selectionCanvas.width = 0;
+canvas.height = selectionCanvas.height = 0;
 var context = canvas.getContext('2d');
+var selectionContext = selectionCanvas.getContext('2d');
 var image = new Image();
 image.onload = function() {
-  canvas.width = image.width;
-  canvas.height = image.height;
+  canvas.width = selectionCanvas.width = image.width;
+  canvas.height = selectionCanvas.height = image.height;
   context.drawImage(image, 0, 0);
 };
+
+var selectionStartX = null;
+var selectionStartY = null;
+var selectionX, selectionY, selectionWidth, selectionHeight = null;
+
+selectionCanvas.addEventListener('mousedown', function(e){
+  console.log('document mousedown');
+  var rect = canvas.getBoundingClientRect();
+  var isInsideCanvas = e.clientX >= rect.left
+                       && e.clientX <= rect.left + rect.width
+                       && e.clientY >= rect.top
+                       && e.clientY <= rect.top + rect.height;
+  console.log('isInsideCanvas: ' + isInsideCanvas);
+  if (isInsideCanvas) {
+    selectionStartX = e.clientX - rect.left;
+    selectionStartY = e.clientY - rect.top;
+  }
+  else {
+    selectionStartX = null;
+    selectionStartY = null;
+  }
+});
+
+document.addEventListener('mouseup', function(e){
+  console.log('document mouseup');
+  if (selectionStartX && selectionStartY) {
+    var rect = canvas.getBoundingClientRect();
+    var selectionEndX = e.clientX - rect.left;
+    var selectionEndY = e.clientY - rect.top;
+    if (selectionEndX < 0)
+      selectionEndX = 0;
+    if (selectionEndY < 0)
+      selectionEndY = 0;
+    if (selectionEndX > rect.width)
+      selectionEndX = rect.width;
+    if (selectionEndY > rect.height)
+      selectionEndY = rect.height;
+    console.log('(' + selectionStartX + ', ' + selectionStartY + ') => ('
+              + selectionEndX + ', ' + selectionEndY + ')');
+
+    selectionX = Math.min(selectionStartX, selectionEndX);
+    selectionY = Math.min(selectionStartY, selectionEndY);
+    selectionWidth = Math.max(selectionStartX, selectionEndX) - selectionX;
+    selectionHeight = Math.max(selectionStartY, selectionEndY) - selectionY;
+
+    selectionContext.clearRect(0, 0, selectionCanvas.width, selectionCanvas.height);
+    selectionContext.beginPath();
+    selectionContext.rect(selectionX + 0.5, selectionY + 0.5,
+                          selectionWidth - 1, selectionHeight - 1);
+    selectionContext.strokeStyle = '#000';
+    selectionContext.setLineDash([5, 4]);
+    selectionContext.lineDashOffset = 0;
+    selectionContext.stroke();
+
+    selectionContext.rect(selectionX + 0.5, selectionY + 0.5,
+                          selectionWidth - 1, selectionHeight - 1);
+    selectionContext.strokeStyle = '#fff';
+    selectionContext.setLineDash([3, 6]);
+    selectionContext.lineDashOffset = 5;
+    selectionContext.stroke();
+
+    selectionStartX = null;
+    selectionStartY = null;
+  }
+});
+
+selectionCanvas.addEventListener('keydown', function(e){
+  console.log('keydown');
+  if (e.keyCode == 27) {
+    selectionX, selectionY, selectionWidth, selectionHeight = null;
+    selectionContext.clearRect(0, 0, selectionCanvas.width, selectionCanvas.height);
+  }
+})
+
 var appMenu = Menu.getApplicationMenu();
 var menuTemplate = [
   {
